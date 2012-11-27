2f2d05ca0cb29e6b4a8d034724bc2d719e0765ed
unknown <hjwk9387@C-BUCP-BC1.rd.francetelecom.fr>
2012-11-27 17:37:36 +0100

    first commit

---
+++ Drawing/js/default.js
@@ -0,0 +1,34 @@
+﻿// Pour obtenir une présentation du modèle Vide, consultez la documentation suivante :
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+    "use strict";
+
+    WinJS.Binding.optimizeBindingReferences = true;
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: cette application vient d'être lancée. Initialisez
+                // votre application ici.
+            } else {
+                // TODO: cette application a été réactivée après avoir été suspendue.
+                // Restaurez l'état de l'application ici.
+            }
+            args.setPromise(WinJS.UI.processAll());
+        }
+    };
+
+    app.oncheckpoint = function (args) {
+        // TODO: cette application est sur le point d'être suspendue. Enregistrez tout état
+        // devant être conservé lors des suspensions ici. Vous pouvez utiliser l'objet
+        // WinJS.Application.sessionState, qui est automatiquement
+        // enregistré et restauré en cas de suspension. Si vous devez effectuer une
+        // opération asynchrone avant la suspension de votre application, appelez
+        // args.setPromise().
+    };
+
+    app.start();
+})();
---
+++ README.md
@@ -0,0 +1,3 @@
+This app is a simple windows 8 drawing application based on HTML5 (mainly on canvas) built during  An [Kings’ Night](http://elsoufy.blogspot.fr/2012/09/tweleve-hours-for-developing-windows-8.html "Title") on Septomber 2012. 
+With this app, kids can have fun coloring a black-and-white reference picture (chosen from the app images library).
+ 
